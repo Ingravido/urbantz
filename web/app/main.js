bd2ac1f52ed7d2beabbe7dbdfe4925ab61945ca5
Javier Del saz <javierdelsaz@Javiers-MacBook-Pro.local>
2020-08-06 00:06:23 +0200

    web: improved showing results on edge cases

--- web/app/main.js
+++ web/app/main.js
@@ -2,14 +2,20 @@ angular
     .module("ohm-delivery", [])
     .controller("tracking", function($scope, $http) {
         $scope.sendData = function() {
-            $http.get(`/ohms/order/${this.trackingId}`)
-            .then((result) => {
-                this.errorMessage = '';
-                if (Object.keys(result.data).length) {
-                    this.orderDetails = result
-                }
-            }, (error) => {
-                this.errorMessage = 'Oops, this website is under construction, please come back later.';
-            });
+            if (this.trackingId && this.trackingId.length) {
+                $http.get(`/ohms/order/${this.trackingId}`)
+                .then((result) => {
+                    if (Object.keys(result.data).length) {
+                        this.orderDetails = result;
+                        this.errorMessage = '';
+                    } else {
+                        this.errorMessage = 'Matching tracking id order not found.';
+                        this.orderDetails = ''
+                    }
+                }, (error) => {
+                    this.errorMessage = 'Oops, this website is under construction, please come back later.';
+                    this.orderDetails = ''
+                });
+            }
         };
     });
