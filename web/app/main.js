c7c9820edbf79e0740b24dd633e6b40d5b4202c1
Javier Del saz <javierdelsaz@Javiers-MacBook-Pro.local>
2020-08-06 17:52:06 +0200

    web: Added texarea to save refused reason

--- web/app/main.js
+++ web/app/main.js
@@ -1,5 +1,7 @@
-angular
-    .module("ohm-delivery", [])
+const trackingApp = angular
+    .module("ohm-delivery", []);
+
+trackingApp
     .controller("tracking", function($scope, $http) {
         $scope.sendData = function() {
             if (this.trackingId && this.trackingId.length) {
@@ -17,5 +19,23 @@ angular
                     this.orderDetails = ''
                 });
             }
+        };
+    })
+
+trackingApp
+    .controller("orderFinalStatus", function($scope, $http) {
+        $scope.status="DELIVERED"
+        $scope.sendData = function() {
+            const data = {
+                status: this.status,
+                comment: this.comment
+            };
+            $http.post(`/ohms/conclude-order/${this.trackingId}`, data).then((result) => {
+                this.errorMessage = '';
+                this.confirmMessage = 'Order updated.'
+            }, (error) => {
+                this.errorMessage = 'Oops, an error occurred.';
+                this.confirmMessage = ''
+            });
         };
     });
